d627e1cdb1613ab234de40a6f088e7ee39eaa5a9
vchraf <vchraf@gmail.com>
2022-02-20 08:39:40 +0100

    v.0

---
+++ LinearRegression.py
@@ -0,0 +1,18 @@
+import numpy
+import math
+
+
+class LinearRegression():
+    def __init__(self) -> None:
+        pass
+
+
+def standardize(X):
+    X_std = X
+    mean = X.mean(axis=0)
+    std = X.std(axis=0)
+    for col in range(X.shape[1]):
+        if std[col]:
+            X_std[:, col] = (X_std[:, col] - mean[col]) / std[col]
+    # X_std = (X - X.mean(axis=0)) / X.std(axis=0)
+    return X_std
